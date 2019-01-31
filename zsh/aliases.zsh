ea21443a4f3ddd2a5e7a8eae633e2cc6070a5488
Christian Whitted <TheVoidLives@users.noreply.github.com>
2019-01-31 11:12:51 -0500

    begin cleanup

---
+++ zsh/aliases.zsh
@@ -0,0 +1,15 @@
+# List of custom Aliases for use with zsh
+
+alias zshconfig="vim ~/.zshrc"
+alias wpaper="open ~/.env/wallpaper"
+alias uantigen='antigen update; antigen cleanup;'
+alias vubrew='brew update -v; brew upgrade -v; brew cask upgrade -v; brew cleanup --prune=0 -s; brew prune -v; brew doctor -v;'
+alias ubrew='brew update; brew upgrade; brew cask upgrade; brew cleanup --prune=0 -s; brew prune; brew doctor;'
+alias adventuretime='emacs -batch -l dunnet'
+alias fcl='fortune | cowsay | lolcat'
+alias gdb='sudo gdb'
+alias 'pip ls'='pip list'
+alias vim='nvim'
+alias go-school="cd ~/Desktop/Schooling/UCF/"
+
+alias :q=exit
