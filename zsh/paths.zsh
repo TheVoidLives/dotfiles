9e58eaa5941280155c2545d016468789a09c86f8
Christian Whitted <TheVoidLives@users.noreply.github.com>
2019-02-16 20:33:03 -0500

    cleanup again

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,13 +1,14 @@
 # Custom Paths
 
 
-export EDITOR="vim"
+export EDITOR="nvim"
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
 export PATH="~/bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export JAVA_HOME="$HOME/.jenv/versions/`jenv version-name`"
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
