ae111248bc680f6f95dbfaf33a18619c0cba72ce
Christian Whitted <TheVoidLives@users.noreply.github.com>
2019-02-26 18:51:45 -0500

    simple update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,13 +3,12 @@
 alias zshconfig="vim ~/.zshrc"
 alias wpaper="open ~/.env/wallpaper"
 alias uantigen='antigen update; antigen cleanup;'
-alias vubrew='brew update -v; brew upgrade -v; brew cask upgrade -v; brew cleanup --prune=0 -s; brew prune -v; brew doctor -v;'
-alias ubrew='brew update; brew upgrade; brew cask upgrade; brew cleanup --prune=0 -s; brew prune; brew doctor;'
+alias vubrew='brew update -v; brew upgrade -v; brew cask upgrade -v; brew cleanup --prune=0 -s; brew doctor -v;'
+alias ubrew='brew update; brew upgrade; brew cask upgrade; brew cleanup --prune=0 -s; brew doctor;'
 alias adventuretime='emacs -batch -l dunnet'
 alias fcl='fortune | cowsay | lolcat'
 alias gdb='sudo gdb'
 alias 'pip ls'='pip list'
 alias vim='nvim'
-alias go-school="cd ~/Desktop/Schooling/UCF/"
 
 alias :q=exit
