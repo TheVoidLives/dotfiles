b097e03fd892f2b5ae1cbc7d901487e6be558b60
Christian Whitted <TheVoidLives@users.noreply.github.com>
2019-08-10 15:48:52 -0400

    rando update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias zshconfig="vim ~/.zshrc"
 alias wpaper="open ~/.env/wallpaper"
 alias uantigen='antigen update; antigen cleanup;'
 alias vubrew='brew update -v; brew upgrade -v; brew cask upgrade -v; brew cleanup --prune=0 -s; brew doctor -v;'
-alias ubrew='brew update; brew upgrade; brew cask upgrade; brew cleanup --prune=0 -s; brew doctor;'
+alias ubrew='brew update && brew upgrade && brew cask upgrade && brew cleanup --prune=0 -s && brew doctor;'
 alias adventuretime='emacs -batch -l dunnet'
 alias fcl='fortune | cowsay | lolcat'
 alias gdb='sudo gdb'
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,9 +1,13 @@
 # Custom Paths
 
-
+# Vim
 export EDITOR="nvim"
+
+# Java & Jenv
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
+
+# Path
 export PATH="~/bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
@@ -13,3 +17,12 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# Golang
+export GOPATH="${HOME}/.go"
+export GOROOT="$(brew --prefix golang)/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+test -d "${GOPATH}" || mkdir "${GOPATH}"
+test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
+
+export PATH="/usr/local/sbin:$PATH"
